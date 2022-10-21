267c1980cb5ed842a2127ea7c3e2b41ad35bef0c
tianazmah <64625379+tianazmah@users.noreply.github.com>
2022-10-21 20:35:38 +0100

    strmapi done

--- cursus/cursus42/libft/libft.h
+++ cursus/cursus42/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hnait <hnait@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 02:31:51 by hnait             #+#    #+#             */
-/*   Updated: 2022/10/20 16:53:15 by hnait            ###   ########.fr       */
+/*   Updated: 2022/10/21 20:19:13 by hnait            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);  // check restrict
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char 	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 size_t	ft_strlcat(char *dst, const char *src,
