64f5eb287a052a1ac0fe88baae2fc94b083c3872
tianazmah <64625379+tianazmah@users.noreply.github.com>
2022-10-28 20:05:19 +0100

    lst understood

--- cursus/cursus42/libft/ft_lstdelone.c
+++ cursus/cursus42/libft/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: hnait <hnait@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 14:59:59 by hnait             #+#    #+#             */
-/*   Updated: 2022/10/23 18:33:08 by hnait            ###   ########.fr       */
+/*   Updated: 2022/10/28 20:04:48 by hnait            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,12 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (!lst || !del)
 		return ;
-	lst->next = 0;
 	del(lst->content);
 	free(lst);
-	lst = 0;
 }	
 
-void fre(void *content)
+void	fre(void *content)
 {
-	content = 0;
 	free(content);
-}
-
-int main()
-{
-	t_list *list = ft_lstnew(ft_strdup("othmane"));
-	ft_lstadd_back(&list, ft_lstnew(ft_strdup("tian")));
-
-	printf("%d\n", ft_lstsize(list));
-	ft_lstdelone(list->next, &fre);
-	printf("%d\n", ft_lstsize(list));
+	printf("ok f");
 }
